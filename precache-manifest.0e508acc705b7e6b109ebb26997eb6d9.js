2e2cd4795a338800a2625d5f2bdc0846cbf334fe
Manuel Corona <coronamvirgo@gmail.com>
2020-03-07 13:52:34 -0500

    Updates

---
+++ precache-manifest.0e508acc705b7e6b109ebb26997eb6d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9facad0dc0080d35472509ca4e27b8",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "9641e092a73eb1e37334",
+    "url": "/Clicky-Game/static/css/main.76c3c3c3.chunk.css"
+  },
+  {
+    "revision": "8b921c9a020d2e46fcae",
+    "url": "/Clicky-Game/static/js/2.af57168b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Clicky-Game/static/js/2.af57168b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9641e092a73eb1e37334",
+    "url": "/Clicky-Game/static/js/main.784741a9.chunk.js"
+  },
+  {
+    "revision": "6e499a457040b02c082b",
+    "url": "/Clicky-Game/static/js/runtime-main.fba16f88.js"
+  }
+]);
